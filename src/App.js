b82a0d6133ccbdecbc47b3ed00bec0307059544b
hadysoufan <hadisoufan74@gmail.com>
2022-09-22 16:01:21 +0300

    rendering the private route and importing toastify pakage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar.component";
-import Explore from "./components/pages/Explore";
-import Offers from "./components/pages/Offers.component";
-import Profile from "./components/pages/Profile.component";
-import SignIn from "./components/pages/Sign-in.component";
-import SignUp from "./components/pages/Sign-up.component";
-import ForgotPassword from "./components/pages/Forgot-password.component";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar.component';
+import PrivateRoute from './components/PrivateRoute.component';
+import Explore from './components/pages/Explore';
+import Offers from './components/pages/Offers.component';
+import Profile from './components/pages/Profile.component';
+import SignIn from './components/pages/Sign-in.component';
+import SignUp from './components/pages/Sign-up.component';
+import ForgotPassword from './components/pages/Forgot-password.component';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -14,7 +17,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element=<ForgotPassword /> />
@@ -22,6 +27,8 @@ function App() {
         {/* Nav Bar */}
         <Navbar />
       </Router>
+
+      <ToastContainer />
     </>
   );
 }
