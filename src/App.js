550351df27493a579188edb880a4bff4d6839057
hadysoufan <hadisoufan74@gmail.com>
2022-09-22 13:41:45 +0300

    creating react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar.component";
+import Explore from "./components/pages/Explore";
+import Offers from "./components/pages/Offers.component";
+import Profile from "./components/pages/Profile.component";
+import SignIn from "./components/pages/Sign-in.component";
+import SignUp from "./components/pages/Sign-up.component";
+import ForgotPassword from "./components/pages/Forgot-password.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element=<ForgotPassword /> />
+        </Routes>
+        {/* Nav Bar */}
+        <Navbar />
+      </Router>
+    </>
   );
 }
 
