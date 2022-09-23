6a834c1db0fd3b1673649068b21e1d0a53e1387e
hadysoufan <hadisoufan74@gmail.com>
2022-09-23 09:37:20 +0300

    Importing CreateListing page

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import Profile from './components/pages/Profile.component';
 import SignIn from './components/pages/Sign-in.component';
 import SignUp from './components/pages/Sign-up.component';
 import ForgotPassword from './components/pages/Forgot-password.component';
+import Category from './components/pages/Category';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import CreateListing from './components/pages/CreateListing.page';
 
 function App() {
   return (
@@ -17,12 +19,14 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element=<ForgotPassword /> />
+          <Route path="/create-listing" element=<CreateListing /> />
         </Routes>
         {/* Nav Bar */}
         <Navbar />
